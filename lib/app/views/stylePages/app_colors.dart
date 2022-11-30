e0a6e0285e332bca28af002732b031010b262099
WilliamDCGomes <williamdouglasgomes@hotmail.com>
2022-11-30 12:27:06 -0300

    Construido estrutura inicial do projeto

---
+++ lib/app/views/stylePages/app_colors.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static final Color whiteColor = Colors.white;
+  static final Color blackColor = Colors.black;
+  static final Color blackTransparentColor = Colors.black.withOpacity(.6);
+  static final Color black40TransparentColor = Colors.black.withOpacity(.4);
+  static final Color blackWithOpacity = Colors.black.withOpacity(.6);
+  static final Color transparentColor = Colors.transparent;
+}
